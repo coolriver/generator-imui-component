421d6a0e9edfcb74cd3c89efbdcf21f532846188
riverhan(韩江) <riverhan@tencent.com>
2016-09-27 16:05:13 +0800

    test: 测试用例去掉eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 'use strict';
 var path = require('path');
 var gulp = require('gulp');
-var eslint = require('gulp-eslint');
 var excludeGitignore = require('gulp-exclude-gitignore');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
@@ -11,10 +10,7 @@ var coveralls = require('gulp-coveralls');
 
 gulp.task('static', function () {
   return gulp.src('**/*.js')
-    .pipe(excludeGitignore())
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
+    .pipe(excludeGitignore());
 });
 
 gulp.task('nsp', function (cb) {
